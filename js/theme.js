ecd5f4f276bc867458ee5f1f78b2a212043afb18
Denis Ivanchenko <super.wilden5@gmail.com>
2026-09-16 17:52:08 +0300

    refactor: constants naming

--- js/theme.js
+++ js/theme.js
@@ -1,18 +1,21 @@
+const LIGHT_THEME = 'light';
+const DARK_THEME = 'dark';
+
 document.addEventListener("DOMContentLoaded", () => {
-    const html = document.documentElement;
-    const lightButton = document.querySelector('.light-theme-button');
-    const darkButton = document.querySelector('.dark-theme-button');
+    const HTML = document.documentElement;
+    const LIGHT_BUTTON = document.querySelector('.light-theme-button');
+    const DARK_BUTTON = document.querySelector('.dark-theme-button');
 
     const applyTheme = (theme) => {
-        html.setAttribute('data-theme', theme);
+        HTML.setAttribute('data-theme', theme);
         localStorage.setItem('theme', theme);
-        lightButton.classList.toggle('active', theme === 'light');
-        darkButton.classList.toggle('active', theme === 'dark');
+        LIGHT_BUTTON.classList.toggle('active', theme === LIGHT_THEME);
+        DARK_BUTTON.classList.toggle('active', theme === DARK_THEME);
     }
 
-    const savedTheme = localStorage.getItem('theme') || 'light';
-    applyTheme(savedTheme);
+    const SAVED_THEME = localStorage.getItem('theme') || LIGHT_THEME;
+    applyTheme(SAVED_THEME);
 
-    lightButton.addEventListener('click', () => applyTheme('light'));
-    darkButton.addEventListener('click', () => applyTheme('dark'));
+    LIGHT_BUTTON.addEventListener('click', () => applyTheme(LIGHT_THEME));
+    DARK_BUTTON.addEventListener('click', () => applyTheme(DARK_THEME));
 })
