f9aa8ea9d91ac0596ccd30eb844a22c28bbaaa48
Denis Ivanchenko <super.wilden5@gmail.com>
2026-09-17 16:52:17 +0300

    feat: implement touch functionality for slider

--- js/carousel.js
+++ js/carousel.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const SLIDER_CARDS = document.querySelector('.slider__cards');
     const SLIDER_CARD = document.querySelectorAll('.slider__card');
+    const SLIDER_TRACK = document.querySelector('.slider__track');
     const PREVIOUS_BUTTON = document.querySelector('.slider__left');
     const NEXT_BUTTON = document.querySelector('.slider__right');
     const INDICATORS = document.querySelectorAll('.progress-bar__item');
 
     let currentSlideIndex = 0;
+    let startX = 0;
 
     const updateSliderPosition = () => {
         SLIDER_CARDS.style.transform = `translateX(-${currentSlideIndex * 100}%)`;
@@ -33,8 +35,21 @@ document.addEventListener('DOMContentLoaded', () => {
         updateIndicators();
     }
 
+    const handleTouchStart = (event) => {
+        startX = event.touches[0].clientX;
+    }
+
+    const handleTouchEnd = (event) => {
+        const diff = startX - event.changedTouches[0].clientX;
+        if (Math.abs(diff) > 50) {
+            diff > 0 ? nextSlide() : previousSlide();
+        }
+    }
+
     NEXT_BUTTON.addEventListener('click', nextSlide);
     PREVIOUS_BUTTON.addEventListener('click', previousSlide);
+    SLIDER_TRACK.addEventListener('touchstart', handleTouchStart);
+    SLIDER_TRACK.addEventListener('touchend', handleTouchEnd);
 
     updateIndicators();
 })
