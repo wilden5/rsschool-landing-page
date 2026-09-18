a428880b0d75adebc43249bd15058ecfe8039467
Denis Ivanchenko <super.wilden5@gmail.com>
2026-09-18 12:47:21 +0300

    feat: add burger button active state styles; animation

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const BURGER_BUTTON = document.querySelector('.burger-icon');
+    const BURGER_BUTTON = document.querySelector('.burger-button');
     const MAIN_NAV = document.querySelector('.main-nav');
 
     BURGER_BUTTON.addEventListener('click', () => {
         MAIN_NAV.classList.toggle('active');
+        BURGER_BUTTON.classList.toggle('active');
 
         if (MAIN_NAV.classList.contains('active')) {
             document.body.style.overflow = 'hidden';
